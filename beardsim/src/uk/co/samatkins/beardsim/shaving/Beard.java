91c7ad594034cf4025756e1015f1bd9918d3b0a8
Sam Atkins <github@samatkins.co.uk>
2013-06-20 14:17:44 +0100

    Using Hair class for hair length and rendering.

--- beardsim/src/uk/co/samatkins/beardsim/shaving/Beard.java
+++ beardsim/src/uk/co/samatkins/beardsim/shaving/Beard.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class Beard extends Entity {
 	
-	private float[][] hairs;
+	private Hair[][] hairs;
 	private boolean[][] canGrow;
 	private int hairsX;
 	private int hairsY;
@@ -29,16 +29,36 @@ public class Beard extends Entity {
 		hairsX = (int) Math.ceil(getWidth() / hairSpacing);
 		hairsY = (int) Math.ceil(getHeight() / hairSpacing);
 		
-		hairs = new float[hairsX][hairsY];
+		hairs = new Hair[hairsX][hairsY];
 		canGrow = new boolean[hairsX][hairsY];
-		for (int i=0; i<hairsX; i++) {
-			for (int j=0; j<hairsY; j++) {
-				hairs[i][j] = 10;
-				canGrow[i][j] = true;
+//		for (int i=0; i<hairsX; i++) {
+//			for (int j=0; j<hairsY; j++) {
+//				hairs[i][j] = 10;
+//				canGrow[i][j] = true;
+//			}
+//		}
+//		
+		applyMask(mask, mouth);
+	}
+	
+	/**
+	 * Mark all areas outside the mask area as places that hair cannot grow.
+	 * @param mask
+	 */
+	public void applyMask(Polygon mask, Polygon mouth) {
+		float xPos, yPos;
+		for (int x = 0; x < hairsX; x++) {
+			xPos = getX() + (x*hairSpacing);
+			for (int y = 0; y < hairsY; y++) {
+				yPos = getY() + (y*hairSpacing);
+				if (mask.contains(xPos, yPos) && !mouth.contains(xPos, yPos)) {
+					canGrow[x][y] = true;
+					hairs[x][y] = new Hair(10);
+				} else {
+					canGrow[x][y] = false;
+				}
 			}
 		}
-		
-		applyMask(mask, mouth);
 	}
 	
 	@Override
@@ -55,20 +75,22 @@ public class Beard extends Entity {
 		float xOff, yOff;
 		for (int i=0; i<hairsX; i++) {
 			x1 = getX() + (hairSpacing * i);
-			x2 = x1;// + hairSpacing/2;
+//			x2 = x1;// + hairSpacing/2;
 			
 			for (int j=0; j<hairsY; j++) {
 				if (canGrow(i,j)) {
 					y1 = getY() + (hairSpacing * j);
-					y2 = y1 - hairs[i][j];
+//					y2 = y1 - hairs[i][j].getLength();
 					
 					xOff = (float) (hairSpacing/2 * Math.sin(i*j));
 					yOff = (float) (hairSpacing/2 * Math.cos(i*j));
+					
+					hairs[i][j].draw(shapeRenderer, x1 + xOff, y1 + yOff);
 							
-					shapeRenderer.line(
-							x1 + xOff, y1 + yOff,
-							x2 + xOff, y2 + yOff
-					);
+//					shapeRenderer.line(
+//							x1 + xOff, y1 + yOff,
+//							x2 + xOff, y2 + yOff
+//					);
 				}
 			}
 		}
@@ -86,7 +108,7 @@ public class Beard extends Entity {
 		for (int i=0; i<hairsX; i++) {
 			for (int j=0; j<hairsY; j++) {
 				if (canGrow(i,j)) {
-					hairs[i][j] += amount;
+					hairs[i][j].grow(amount);
 				}
 			}
 		}
@@ -109,12 +131,7 @@ public class Beard extends Entity {
 			return;
 		}
 		
-		if (amount >= hairs[x][y]) {
-			hairs[x][y] = 0;
-			return;
-		}
-		
-		hairs[x][y] -= amount;
+		hairs[x][y].shave(amount);
 	}
 
 	public void shave(RotatedRectangle area) {
@@ -136,26 +153,6 @@ public class Beard extends Entity {
 	}
 	
 	/**
-	 * Mark all areas outside the mask area as places that hair cannot grow.
-	 * @param mask
-	 */
-	public void applyMask(Polygon mask, Polygon mouth) {
-		float xPos, yPos;
-		for (int x = 0; x < hairsX; x++) {
-			xPos = getX() + (x*hairSpacing);
-			for (int y = 0; y < hairsY; y++) {
-				yPos = getY() + (y*hairSpacing);
-				if (mask.contains(xPos, yPos) && !mouth.contains(xPos, yPos)) {
-					canGrow[x][y] = true;
-				} else {
-					canGrow[x][y] = false;
-					hairs[x][y] = 0;
-				}
-			}
-		}
-	}
-	
-	/**
 	 * Calculates how symmetrical the beard is, and returns a value between 0 and 1
 	 * @return 0 = not at all symmetrical, 1 = perfect symmetry
 	 */
@@ -170,7 +167,12 @@ public class Beard extends Entity {
 		for (int i=0; i<difference.length; i++) {
 			for (int j=0; j<hairsY; j++) {
 				if (canGrow(i,j)) {
-					difference[i][j] = Math.abs(hairs[i][j] - hairs[hairsX-i-1][j]);
+					difference[i][j] = Math.abs(
+							hairs[i][j].getLength()
+							- (canGrow(hairsX-i-1, j)
+									? hairs[hairsX-i-1][j].getLength()
+									: 0)
+					);
 					totalArea++;
 				}
 			}
@@ -189,8 +191,17 @@ public class Beard extends Entity {
 	}
 	
 	public class Hair {
-		public float length;
-		public float angle;
+		private float length;
+		private float angle;
+		
+		public Hair(float length) {
+			this(length, 270);
+		}
+		
+		public Hair(float length, float angle) {
+			this.length = length;
+			this.angle = angle;
+		}
 		
 		public void draw(ShapeRenderer shapeRenderer, float x, float y) {
 
@@ -198,6 +209,27 @@ public class Beard extends Entity {
 					y2 = y - length;
 					
 			shapeRenderer.line( x, y, x2, y2 );
+		}
+		
+		public float getLength() {
+			return length;
+		}
+		
+		public float getAngle() {
+			return angle;
+		}
+		
+		public void grow(float amount) {
+			length += amount;
+		}
+		
+		public void shave(float amount) {
+			if (amount >= length) {
+				length = 0;
+				return;
+			}
+			
+			length -= amount;
 		}
 	}
 }
