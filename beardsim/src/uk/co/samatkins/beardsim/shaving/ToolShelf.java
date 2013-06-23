2573d9c1d4c4f5720d41452c022c01802b3b26ce
Sam Atkins <github@samatkins.co.uk>
2013-06-23 15:22:39 +0100

    Improved razor graphic.

--- beardsim/src/uk/co/samatkins/beardsim/shaving/ToolShelf.java
+++ beardsim/src/uk/co/samatkins/beardsim/shaving/ToolShelf.java
@@ -1,16 +1,13 @@
 package uk.co.samatkins.beardsim.shaving;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public class ToolShelf extends Table {
 	
+	@SuppressWarnings("unused")
 	private ButtonGroup buttonGroup;
 
 	public ToolShelf(ShavingScene shavingScene, Skin skin) {
@@ -42,15 +39,6 @@ public class ToolShelf extends Table {
 			
 			this.shavingScene = shavingScene;
 			this.tool = shavingScene.getTool(toolName);
-			
-			addListener(new ClickListener() {
-				@Override
-				public void clicked(InputEvent event, float x, float y) {
-					// TODO Auto-generated method stub
-					super.clicked(event, x, y);
-					Gdx.app.debug("ToolButton", "Clicked!");
-				}
-			});
 		}
 		
 		@Override
